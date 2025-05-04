97c366f278616d6ea6dc3f1be244072448c3fb55
PROgrammer <97875562+Walnusskeim@users.noreply.github.com>
2025-05-04 13:19:42 +0200

    Now able to send one time via app
    Why am I so goated it doesn't make any sense.

--- KiddoVA.py
+++ KiddoVA.py
@@ -12,7 +12,6 @@ import requests
 
 import kivy
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
@@ -22,10 +21,25 @@ from kivy.core.window import Window
 ##################################################
 
 kivy.require('2.0.0')
-Builder.load_file("kiddova.kv")
+
 
 class MyLayout(Widget):
     pass
+
+
+
+def set_alarm(time):
+    url = "http://10.0.0.69:6969/alarm"
+    payload = {"time": time}
+
+    try:
+        response = requests.post(url, json=payload)
+        if response.status_code == 200:
+            print("Es klingelt um:", response.json().get("time"))
+        else:
+            print("Fehler:", response.json().get("error"), "Code:", response.status_code)
+    except Exception as e:
+        print("Verbindung fehlgeschlagen:", e)
 
 
 class KiddoVA(App):
@@ -33,7 +47,6 @@ class KiddoVA(App):
         Window.clearcolor = (0.1, 0.1, 0.1, 1)
         return MyLayout()
 
+
 if __name__ == '__main__':
     KiddoVA().run()
-
-
